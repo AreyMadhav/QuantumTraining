a7ab916dfd9edac44fc1828c4fa974943c477403
Madhav Tyagi <realmadhavtyagi@gmail.com>
2024-02-20 03:57:32 +0530

    Update BellStates.py

--- QuantumDemo/BellStates.py
+++ QuantumDemo/BellStates.py
@@ -1,17 +1,14 @@
-from pyquil import Program, get_qc
-from pyquil.gates import *
-from pyquil.quilbase import Declare
+from pyquil import get_qc, Program
+from pyquil.gates import CNOT, H, MEASURE
 
-p = Program(
-    Declare("ro", "BIT", 2),
-    H(0),
-    CNOT(0, 1),
-    MEASURE(0, ("ro", 0)),
-    MEASURE(1, ("ro", 1)),
-).wrap_in_numshots_loop(10)
+qvm = get_qc('2q-qvm')  # Use a 2-qubit quantum virtual machine
+p = Program()
+p += H(0)  # Apply Hadamard gate to qubit 0
+p += CNOT(0, 1)  # Apply CNOT gate between qubits 0 and 1
+ro = p.declare('ro', 'BIT', 2)  # Declare classical memory
+p += MEASURE(0, ro[0])  # Measure qubit 0
+p += MEASURE(1, ro[1])  # Measure qubit 1
+p.wrap_in_numshots_loop(10)  # Repeat the experiment 10 times
 
-# run the program on a QVM
-qc = get_qc('9q-square-qvm')
-result = qc.run(qc.compile(p)).get_register_map().get("ro")
-print(result[0])
-print(result[1])
+results = qvm.run(p).get_register_map()['ro'].tolist()
+print(results)
